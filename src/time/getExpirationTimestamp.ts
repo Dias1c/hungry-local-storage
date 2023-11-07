36850ff4ee73ee6b9e2143991685feb00252ab9d
Dias1c <dias.kappasov@gmail.com>
2023-11-07 18:01:35 +0600

    upd: getExpirationTimestamp: comments

--- src/time/getExpirationTimestamp.ts
+++ src/time/getExpirationTimestamp.ts
@@ -2,6 +2,11 @@ import { DurationInSeconds } from "./DurationInSeconds.js"
 import { getCurrentTimestamp } from "./getCurrentTimestamp.js"
 import { TExpiration } from "./types.js"
 
+/**
+ * Returns a [`unix timestamp`](https://www.unixtimestamp.com/) based on the received data. If `expiration` type is number, returns this value, otherwise **if it's object**, it adds time to current timestamp (now + seconds + days + hours + ...). 
+ * 
+ * @returns A `unix timestamp` representing the future expiration time.
+ */
 export function getExpirationTimestamp(expiration: TExpiration): number {
   if (typeof expiration === 'number') return expiration
 
